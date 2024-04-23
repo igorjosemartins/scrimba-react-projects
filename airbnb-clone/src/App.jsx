7fb1c7f85d058c5e193c15c3a495f50348d2ac80
Igor Martins <igorjosemartins01@gmail.com>
2024-04-23 19:38:54 -0300

    airbnb clone : navbar + hero

---
+++ airbnb-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
